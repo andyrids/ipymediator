78d7c37cfd4bfcac7f88142e6779bfb1ed77ab1e
Andrew Ridyard <ridyard12@gmail.com>
2024-04-30 18:51:27 +0000

    chore: delete folder

--- ipymediator/__init__.py
+++
@@ -1,4 +0,0 @@
-from ipydialogs.__version__ import __version__
-from ipydialogs.dialogs import FileDialog
-
-__all__ = ("FileDialog", "__version__")
--- ipymediator/interface/__init__.py
+++
@@ -1,3 +0,0 @@
-from .component import Component
-from .mediator import DialogMediator, Mediator
-from .metaclass import ABCTraitsMeta, MetaHasTraits
