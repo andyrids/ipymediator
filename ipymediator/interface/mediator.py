1fbd89b0695ad0846ca9ba3d703271bcaa6dca37
Andrew Ridyard <ridyard12@gmail.com>
2024-05-16 20:54:30 +0000

    chore: move MediatorWithTraits class to interface module

--- ipymediator/interface/mediator.py
+++ ipymediator/interface/mediator.py
@@ -1,11 +1,9 @@
-import pathlib
 from abc import abstractmethod
 from typing import Union
 
 from ipymediator.interface.metaclass import ABCTraits
 from ipymediator.enumerations import Value, Options
-
-from traitlets import Bool, HasTraits, Instance
+from traitlets import HasTraits
 
 
 class Mediator(ABCTraits):
@@ -23,27 +21,11 @@ class Mediator(ABCTraits):
 
             change (Mapping): Trait changes from DOMWidget observe function
         """
-        pass
 
 
-class DialogMediator(Mediator, HasTraits):
-    """Abstract Mediator class for Mediator interface implimentation. A
-    DialogMediator extends the Mediator interface implimentation by adding
-    traitlet library traits.
-
-    Traits:
-        dialog_open (bool): Dialog open/closed flag
-
-        dialog_selection (pathlib.Path): Selected file path
-
-    Abstract methods:
-        notify: From Mediator class
-
-        _control: Holds a reference to a DialogMediator control Mediator
+class MediatorWithTraits(Mediator, HasTraits):
+    """Abstract Mediator class for Mediator interface implimentation. Extends
+    the Mediator interface by allowing traits from the traitlets library,
+    through HasTraits inheritence.
     """
-
-    dialog_open = Bool(default_value=True).tag(sync=True)  # type: ignore
-    dialog_selection = Instance(
-        pathlib.Path,
-        allow_none=True,
-        default_value=None).tag(sync=True)  # type: ignore
+    pass
